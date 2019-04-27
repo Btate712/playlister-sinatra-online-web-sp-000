463104564b71f5a998a5624a4056a3403e208c6b
Robert Tate <btate712@gmail.com>
2019-04-27 11:45:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < Sinatra::Base
     else
       @song = Song.find_by_slug(params[:id])
     end
-    binding.pry
+    
     if @song == Song.all.last
       @new_song_message = true
     end
